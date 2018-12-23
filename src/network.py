342dec052cf420f6ca3e94c3a07172b6626ae2a4
Zhang Duo <zhangduo2015@yahoo.co.jp>
2018-12-23 23:01:34 +0900

    Update network.py

--- src/network.py
+++ src/network.py
@@ -7,6 +7,10 @@ algorithm for a feedforward neural network.  Gradients are calculated
 using backpropagation.  Note that I have focused on making the code
 simple, easily readable, and easily modifiable.  It is not optimized,
 and omits many desirable features.
+
+一种实现前馈神经网络随机梯度下降学习算法的模块。 使用反向传播计算梯度。 
+请注意，我专注于使代码简单，易读且易于修改。 它没有经过优化，省略了许多理想的功能。
+
 """
 
 #### Libraries
@@ -19,7 +23,8 @@ import numpy as np
 class Network(object):
 
     def __init__(self, sizes):
-        """The list ``sizes`` contains the number of neurons in the
+        """
+        The list ``sizes`` contains the number of neurons in the
         respective layers of the network.  For example, if the list
         was [2, 3, 1] then it would be a three-layer network, with the
         first layer containing 2 neurons, the second layer 3 neurons,
@@ -28,7 +33,14 @@ class Network(object):
         distribution with mean 0, and variance 1.  Note that the first
         layer is assumed to be an input layer, and by convention we
         won't set any biases for those neurons, since biases are only
-        ever used in computing the outputs from later layers."""
+        ever used in computing the outputs from later layers.
+        
+        list对象sizes包含了网络各层中神经元的数量。比如，[2,3,1]代表创建
+        一个第一层有2个神经元，第二层3个神经元，第三次1个神经元的3层网络.
+        网络对象中的偏差和权重是初始化为随机值，使用Numpy包中的np.random.randn函数来产生均值为0，标准差为1的⾼斯分布。
+        Network初始化代码中是假定第一层为输入层的，并且对这些神经元不设置任何偏差，因为偏差仅仅被用于后面层输出的计算。
+        """
+        
         self.num_layers = len(sizes)
         self.sizes = sizes
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
@@ -36,21 +48,30 @@ class Network(object):
                         for x, y in zip(sizes[:-1], sizes[1:])]
 
     def feedforward(self, a):
-        """Return the output of the network if ``a`` is input."""
+        """Return the output of the network if ``a`` is input.
+        对于网络给定输入a,返回对应的输出，这个方法是对每一层应用方程a`=σ(wa+b)
+        """
         for b, w in zip(self.biases, self.weights):
-            a = sigmoid(np.dot(w, a)+b)
+            a = sigmoid(np.dot(w, a)+b) # np.dot: 点积
         return a
 
     def SGD(self, training_data, epochs, mini_batch_size, eta,
             test_data=None):
-        """Train the neural network using mini-batch stochastic
+        """
+        Train the neural network using mini-batch stochastic
         gradient descent.  The ``training_data`` is a list of tuples
         ``(x, y)`` representing the training inputs and the desired
         outputs.  The other non-optional parameters are
         self-explanatory.  If ``test_data`` is provided then the
         network will be evaluated against the test data after each
         epoch, and partial progress printed out.  This is useful for
-        tracking progress, but slows things down substantially."""
+        tracking progress, but slows things down substantially.
+        
+        training_data 是⼀个 (x, y) 元组的列表，表⽰训练输⼊和其对应的期望输出。
+        其他非可选参数是不言自明的。变量 epochs 和mini_batch_size 正如你预料的 —— 迭代期数量，和采样时的⼩批量数据的⼤⼩。
+        eta 是学习速率，η。如果给出了可选参数 test_data，那么程序会在每个训练器后评估⽹络，
+        并打印出部分进展。这对于追踪进度很有⽤，但相当拖慢执⾏速度。
+        """
         if test_data: n_test = len(test_data)
         n = len(training_data)
         for j in xrange(epochs):
@@ -67,13 +88,21 @@ class Network(object):
                 print "Epoch {0} complete".format(j)
 
     def update_mini_batch(self, mini_batch, eta):
-        """Update the network's weights and biases by applying
+        """
+        Update the network's weights and biases by applying
         gradient descent using backpropagation to a single mini batch.
         The ``mini_batch`` is a list of tuples ``(x, y)``, and ``eta``
-        is the learning rate."""
+        is the learning rate.
+        
+        在每一次迭代期，先随机排列训练样本，然后将它分成适当大小的小批量数据。
+        这是一个简单的从训练样本随机采样的数据。然后对每一个小批量数据应用一次梯度下降。
+        它仅仅使⽤ mini_batch 中的训练数据，根据单次梯度下降的迭代更新⽹络的权重和偏置。
+        update_mini_batch 的⼯作仅仅是对 mini_batch 中的每⼀个训练样本计算梯度
+        """
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
         for x, y in mini_batch:
+            # 这行是关键，调用了一个称为反向传播的算法，可以快速的计算代价函数的梯度。
             delta_nabla_b, delta_nabla_w = self.backprop(x, y)
             nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
             nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
@@ -137,5 +166,7 @@ def sigmoid(z):
     return 1.0/(1.0+np.exp(-z))
 
 def sigmoid_prime(z):
-    """Derivative of the sigmoid function."""
+    """Derivative of the sigmoid function.
+    sigmoid_prime方法是用来计算σ函数的导数的。
+    """
     return sigmoid(z)*(1-sigmoid(z))
